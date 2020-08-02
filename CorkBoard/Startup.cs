51dd46087f7e64e851cdb53d80d7216c8301c422
MakeIt-Mike <mike.isaac.jones@gmail.com>
2020-08-02 17:45:26 -0500

    cleaning up gui

--- CorkBoard/Startup.cs
+++ CorkBoard/Startup.cs
@@ -29,6 +29,7 @@ namespace CorkBoard
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
